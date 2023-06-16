fe6a69d6c6392ec745139259cd912e195712d284
Gabriel Teiga <gabrielteiga99@gmail.com>
2023-06-16 14:51:12 -0300

    STRATEGY: mais flexivel, reutilizavel. A calculadora esta atrelado a uma interface.

--- src/TesteImpostos.java
+++ src/TesteImpostos.java
@@ -4,6 +4,7 @@ public class TesteImpostos {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
     }
 }
